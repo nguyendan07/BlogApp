bcf80c14712f32858aaee6f88a1874cd54b6e628
Nguyen Dan <nguyendan@hotmail.com.vn>
2017-11-20 15:37:34 +0700

    Add sitemap and create feeds

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     # post views
@@ -8,5 +9,6 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[\w-]+)/$',
         views.post_detail, name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
-    url(r'^tag/(?P<tag_slug>[-\w]+)$', views.post_list, name='post_list_by_tag')
+    url(r'^tag/(?P<tag_slug>[-\w]+)$', views.post_list, name='post_list_by_tag'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
